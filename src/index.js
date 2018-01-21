a86c74a5e237ebdfc90d9e30296a56b4ef05d77d
ronliu <ronliu2k@gmail.com>
2018-01-21 12:21:15 +1100

    minor changes

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const stateLink = withClientState({
 });
 
 const client = new ApolloClient({
-  link: stateLink, //ApolloLink.from([ authLink, stateLink, httpLink]),  //authLink.concat(httpLink),
+  link: stateLink,
   cache
 })
 
